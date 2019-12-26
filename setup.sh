e93682da4e24eb278a63d2ab39a35410bedd1aeb
David Glaeseman <backgroundcolor@gmail.com>
2019-12-26 14:38:21 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ git clone https://github.com/cytopia/devilbox ~/Websites
 cp ~/Websites/env-example ~/Websites/.env
 
 # Setup docker permissions and install docker snap
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-newgrp docker
+#sudo groupadd docker
+#sudo gpasswd -a $USER docker
+#newgrp docker
 sudo snap install docker
 
 
