9dc849619ded9cca23bdcffa517059ff9f32bdf7
David Glaeseman <backgroundcolor@gmail.com>
2019-12-26 15:32:40 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,17 +5,17 @@ sudo apt install curl git zsh vim docker.io -y
 
 # Install OH My ZSH
 #wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s $(which zsh)
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#chsh -s $(which zsh)
 
 # Clone Devilbox
 git clone https://github.com/cytopia/devilbox ~/Websites
 cp ~/Websites/env-example ~/Websites/.env
 
 # Setup docker permissions and install docker snap
-#sudo groupadd docker
-#sudo gpasswd -a $USER docker
-#newgrp docker
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
 sudo snap install docker
 cd ~/Websites
 
