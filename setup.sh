705f49263d11a6e79a7dd469c7007904f176907f
David Glaeseman <backgroundcolor@gmail.com>
2019-12-26 15:44:24 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,12 @@ cp ~/Websites/env-example ~/Websites/.env
 #sudo gpasswd -a $USER docker
 #newgrp docker
 sudo snap install docker
+# Install Docker.io
 sudo apt install docker.io -y
+# Install Docker-Compose
+curl -L https://github.com/docker/compose/releases/download/1.25.1-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 #cd ~/Websites
 
 
