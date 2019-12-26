f30712e0b2ecfbc12dc2e3b7e37cfdd127c91582
David Glaeseman <backgroundcolor@gmail.com>
2019-12-26 14:13:43 -0700

    Update setup.sh
    Devilbox clone

--- setup.sh
+++ setup.sh
@@ -6,8 +6,11 @@ sudo apt install curl git zsh vim -y
 # Install OH My ZSH
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 
-
+# Setup docker permissions and install docker snap
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 newgrp docker
-#sudo snap install docker
+sudo snap install docker
+
+# Clone Devilbox
+git clone https://github.com/cytopia/devilbox ~/Websites
