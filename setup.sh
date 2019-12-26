9513048102c9efc0fea8095bbfcc66109d4e747a
David Glaeseman <backgroundcolor@gmail.com>
2019-12-26 14:52:16 -0700

    Update setup.sh
    trying to debug why docker isn't running

--- setup.sh
+++ setup.sh
@@ -5,12 +5,12 @@ sudo apt install curl git zsh vim -y
 
 # Install OH My ZSH
 #wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s $(which zsh)
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#chsh -s $(which zsh)
 
 # Clone Devilbox
-git clone https://github.com/cytopia/devilbox ~/Websites
-cp ~/Websites/env-example ~/Websites/.env
+#git clone https://github.com/cytopia/devilbox ~/Websites
+#cp ~/Websites/env-example ~/Websites/.env
 
 # Setup docker permissions and install docker snap
 #sudo groupadd docker
