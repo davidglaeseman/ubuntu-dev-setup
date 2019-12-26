22f17b45f632092742ce8b783e7376b48921d9e3
David Glaeseman <backgroundcolor@gmail.com>
2019-12-26 14:03:15 -0700

    Update setup.sh
    Added code for installing oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install curl git zsh vim -y
+
+# Install OH My ZSH
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
