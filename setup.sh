949638f83aacbccd745811a282919d9121a3ad57
David Glaeseman <backgroundcolor@gmail.com>
2019-12-30 09:37:51 -0700

    variables for install and clone

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt upgrade -y
 sudo apt install curl git zsh vim gnome-shell gnome-tweak-tool unity-tweak-tool snapd -y
 
 INSTALL=$1
-if [INSTALL == true]; then
+if [INSTALL == 1]; then
     # Install Default Applications
     sudo snap install phpstorm --classic    #   web development
     sudo snap install bitwarden             #   password manager
@@ -17,10 +17,13 @@ if [INSTALL == true]; then
     sudo apt install ~/Downloads/Hyper.deb
 fi
 
-# Clone the Mojave Dark Theme -- Requires github account
-# mkdir ~/.themes
-# git clone git@github.com:vinceliuice/Mojave-gtk-theme.git ~/Themes/Mojave-Dark
-# cp ~/Themes/Mojave-Dark/src ~/.themes/Mojave-Dark
+GITHUBUSER=$2
+if [GITHUBUSER == 1]; then
+    # Clone the Mojave Dark Theme -- Requires github account
+    mkdir ~/.themes
+    git clone git@github.com:vinceliuice/Mojave-gtk-theme.git ~/Themes/Mojave-Dark
+    cp ~/Themes/Mojave-Dark/src ~/.themes/Mojave-Dark
+fi
 
 # Install Docker
 sudo apt install docker -y
@@ -32,8 +35,7 @@ cp ~/ubuntu-dev-setup/.env ~/Websites/.env
 
 # Install OH My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-echo "zsh" >>> ~/.bashrc
-#chsh -s $(which zsh)
+chsh -s $(which zsh)
 
 # Create a symlink between functions
 ln -s ~/ubuntu-dev-setup/functions.zsh ~/.oh-my-zsh/custom/functions.zsh
