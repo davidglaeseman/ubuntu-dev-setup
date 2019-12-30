a919a0041d016718ed485247fcf313685085065a
David Glaeseman <backgroundcolor@gmail.com>
2019-12-30 09:28:51 -0700

    added flag for install

--- setup.sh
+++ setup.sh
@@ -3,15 +3,19 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install curl git zsh vim gnome-shell gnome-tweak-tool unity-tweak-tool snapd -y
 
-# Install Default Applications
-sudo snap install phpstorm --classic    #   web development
-sudo snap install bitwarden             #   password manager
-sudo snap install postman               #   api development
-sudo snap install atom --classic        #   web development -- free edition if you don't have a phpstorm license
-sudo snap install spotify               #   spotify music
+INSTALL=$1
+if [INSTALL == true]; then
+    # Install Default Applications
+    sudo snap install phpstorm --classic    #   web development
+    sudo snap install bitwarden             #   password manager
+    sudo snap install postman               #   api development
+    sudo snap install atom --classic        #   web development -- free edition if you don't have a phpstorm license
+    sudo snap install spotify               #   spotify music
 
-# Download hyper.js Terminal
-curl -L https://releases.hyper.is/download/deb --output ~/Downloads/Hyper.deb
+    # Download hyper.js Terminal and Install
+    curl -L https://releases.hyper.is/download/deb --output ~/Downloads/Hyper.deb
+    sudo apt install ~/Downloads/Hyper.deb
+fi
 
 # Clone the Mojave Dark Theme -- Requires github account
 # mkdir ~/.themes
@@ -28,7 +32,8 @@ cp ~/ubuntu-dev-setup/.env ~/Websites/.env
 
 # Install OH My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s $(which zsh)
+echo "zsh" >>> ~/.bashrc
+#chsh -s $(which zsh)
 
 # Create a symlink between functions
 ln -s ~/ubuntu-dev-setup/functions.zsh ~/.oh-my-zsh/custom/functions.zsh
