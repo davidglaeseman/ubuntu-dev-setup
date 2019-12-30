602b970d1d1165e9a222e93f543d6377b84652ec
David Glaeseman <backgroundcolor@gmail.com>
2019-12-30 09:54:03 -0700

    fixed if issue

--- setup.sh
+++ setup.sh
@@ -3,26 +3,27 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install curl git zsh vim gnome-shell gnome-tweak-tool unity-tweak-tool snapd -y
 
-INSTALL=$1
-if [INSTALL == 1]; then
-    # Install Default Applications
-    sudo snap install phpstorm --classic    #   web development
-    sudo snap install bitwarden             #   password manager
-    sudo snap install postman               #   api development
-    sudo snap install atom --classic        #   web development -- free edition if you don't have a phpstorm license
-    sudo snap install spotify               #   spotify music
+#!/bin/bash
+INSTALL_APPS=$1
+if [ "$INSTALL_APPS" = true ]; then
+  # Install Default Applications
+  sudo snap install phpstorm --classic    #   web development
+  sudo snap install bitwarden             #   password manager
+  sudo snap install postman               #   api development
+  sudo snap install atom --classic        #   web development -- free edition if you don't have a phpstorm license
+  sudo snap install spotify               #   spotify music
 
-    # Download hyper.js Terminal and Install
-    curl -L https://releases.hyper.is/download/deb --output ~/Downloads/Hyper.deb
-    sudo apt install ~/Downloads/Hyper.deb
+  # Download hyper.js Terminal and Install
+  curl -L https://releases.hyper.is/download/deb --output ~/Downloads/Hyper.deb
+  sudo apt install ~/Downloads/Hyper.deb
 fi
 
-GITHUBUSER=$2
-if [GITHUBUSER == 1]; then
-    # Clone the Mojave Dark Theme -- Requires github account
-    mkdir ~/.themes
-    git clone git@github.com:vinceliuice/Mojave-gtk-theme.git ~/Themes/Mojave-Dark
-    cp ~/Themes/Mojave-Dark/src ~/.themes/Mojave-Dark
+GITHUB_USER=$2
+if [ "$GITHUB_USER" = true ]; then
+  # Clone the Mojave Dark Theme -- Requires github account
+  mkdir ~/.themes
+  git clone git@github.com:vinceliuice/Mojave-gtk-theme.git ~/Themes/Mojave-Dark
+  cp ~/Themes/Mojave-Dark/src ~/.themes/Mojave-Dark
 fi
 
 # Install Docker
